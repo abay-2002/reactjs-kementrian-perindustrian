d3824d6ba9060aea0e1a214a0e9074ba6973d259
Akbar Angkasa <akbar121202@gmail.com>
2023-12-09 21:10:34 +0800

    create dummy

--- src/pages/create/index.jsx
+++ src/pages/create/index.jsx
@@ -1,19 +1,54 @@
+import axios from "axios";
+
 export default function Create() {
+
+    // == Event Handler ==
+    function postHandler() {        
+        let data = JSON.stringify({
+            "name": "Hello From React",
+            "author": "angkasa",
+            "publish_date": "2023-12-5"
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'http://127.0.0.1:8000/api/books',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
         <div className="m-10 flex flex-col gap-6">
             <h1 className="text-3xl font-medium">Create Book</h1>
-            <form>
+            <div>
                 <div className="mb-5">
-                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
-                    <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                    <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
                 </div>
                 <div className="mb-5">
-                    <label for="author" classauthor="block mb-2 text-sm font-medium text-gray-900">author</label>
-                    <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                    <label htmlFor="author" classauthor="block mb-2 text-sm font-medium text-gray-900">author</label>
+                    <input type="text" id="author" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
                 </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Create</button>
-            </form>
-
+                <button
+                    onClick={() => postHandler()}
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                >
+                    Create
+                </button>
+            </div>
         </div>
     )
 }
